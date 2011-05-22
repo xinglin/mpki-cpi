0b54afb7b1b7f239cdc2a72dcfc9a636406b31a4
Xing Lin <xinglin@cs.utah.edu>
2011-05-21 19:05:28 -0600

    final round review for analyze-mpki-ipc-sum4.pl.

--- analyze-mpki-ipc-sum4.pl
+++ analyze-mpki-ipc-sum4.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-# analyze-mpki-ipc-sum4 - analyze the differences in cache partitionings 
+# analyze-mpki-ipc-sum4 - analyze the differences in cache partitions 
 #                         when optimized for MPKI sum or IPC sum,
 #                         based on MPKIs and accurate CPIs 
 #                         for 4-benchmark workloads. 
@@ -83,24 +83,24 @@ foreach $key (keys %programs){
 print "\n\nbegin to calculate combinations of 4-benchmark workloads...\n";
 my @keys = (keys %programs);
 my $key_num = scalar(@keys);
-my ($program1, $program2, $program3,$prog4) = (0,0,0,0); 
+my ($prog1, $prog2, $prog3, $prog4) = (0,0,0,0); 
 my ($mpki_min_i, $mpki_min_j, $mpki_min_k) = (0,0,0);
 my ($same_result, $diff_result) = (0,0);
 my ($length, $mpki_diff, $ipc_diff) = (0,0,0);
 my $output_str = 0;
-for ($program1 = 0; $program1 <= $key_num - 4; $program1++){
-	for($program2 = $program1+1; $program2 <= $key_num - 3 ; $program2++){
-		for($program3 = $program2+1; $program3 <= $key_num - 2 ; $program3++){
-		for($prog4 = $program3+1; $prog4 <= $key_num - 1 ; $prog4++){
+for ($prog1 = 0; $prog1 <= $key_num - 4; $prog1++){
+	for($prog2 = $prog1+1; $prog2 <= $key_num - 3 ; $prog2++){
+		for($prog3 = $prog2+1; $prog3 <= $key_num - 2 ; $prog3++){
+		for($prog4 = $prog3+1; $prog4 <= $key_num - 1 ; $prog4++){
 		($mpki_min_i, $mpki_min_j, $mpki_min_k) 
-							= mpki_min4($MPKIs[$programs{$keys[$program1]}], 
-								$MPKIs[$programs{$keys[$program2]}],
-								$MPKIs[$programs{$keys[$program3]}],
+							= mpki_min4($MPKIs[$programs{$keys[$prog1]}], 
+								$MPKIs[$programs{$keys[$prog2]}],
+								$MPKIs[$programs{$keys[$prog3]}],
 								$MPKIs[$programs{$keys[$prog4]}]);
 		my ($ipc_i, $ipc_j, $ipc_k, $ipc_sum) 
-							= max_ipc_sum4($CPIs[$programs{$keys[$program1]}], 
-								$CPIs[$programs{$keys[$program2]}],
-								$CPIs[$programs{$keys[$program3]}],
+							= max_ipc_sum4($CPIs[$programs{$keys[$prog1]}], 
+								$CPIs[$programs{$keys[$prog2]}],
+								$CPIs[$programs{$keys[$prog3]}],
 								$CPIs[$programs{$keys[$prog4]}]);
 		if($mpki_min_i == $ipc_i && $mpki_min_j == $ipc_j 
 								&& $mpki_min_k == $ipc_k){
@@ -111,44 +111,44 @@ for ($program1 = 0; $program1 <= $key_num - 4; $program1++){
 		}
 	
 		# difference details
-		$length = scalar(@{ $MPKIs[$programs{$keys[$program1]}] });
+		$length = scalar(@{ $MPKIs[$programs{$keys[$prog1]}] });
 		
-		my $mpki_total1 = $MPKIs[$programs{$keys[$program1]}][$mpki_min_i] + 
-				$MPKIs[$programs{$keys[$program2]}][$mpki_min_j] +
-				$MPKIs[$programs{$keys[$program3]}][$mpki_min_k] +
+		my $mpki_total1 = $MPKIs[$programs{$keys[$prog1]}][$mpki_min_i] + 
+				$MPKIs[$programs{$keys[$prog2]}][$mpki_min_j] +
+				$MPKIs[$programs{$keys[$prog3]}][$mpki_min_k] +
 				$MPKIs[$programs{$keys[$prog4]}][$length - $mpki_min_i 
 											- $mpki_min_j - $mpki_min_k - 4];
-		my $mpki_total2 = $MPKIs[$programs{$keys[$program1]}][$ipc_i] + 
-				$MPKIs[$programs{$keys[$program2]}][$ipc_j] + 
-				$MPKIs[$programs{$keys[$program3]}][$ipc_k] + 
+		my $mpki_total2 = $MPKIs[$programs{$keys[$prog1]}][$ipc_i] + 
+				$MPKIs[$programs{$keys[$prog2]}][$ipc_j] + 
+				$MPKIs[$programs{$keys[$prog3]}][$ipc_k] + 
 				$MPKIs[$programs{$keys[$prog4]}][$length - $ipc_i 
 											- $ipc_j -$ipc_k - 4];
 
-		my $ipc_total1 = 1/$CPIs[$programs{$keys[$program1]}][$mpki_min_i] + 
-				1/$CPIs[$programs{$keys[$program2]}][$mpki_min_j] +
-				1/$CPIs[$programs{$keys[$program3]}][$mpki_min_k] +
+		my $ipc_total1 = 1/$CPIs[$programs{$keys[$prog1]}][$mpki_min_i] + 
+				1/$CPIs[$programs{$keys[$prog2]}][$mpki_min_j] +
+				1/$CPIs[$programs{$keys[$prog3]}][$mpki_min_k] +
 				1/$CPIs[$programs{$keys[$prog4]}][$length - $mpki_min_i 
 											- $mpki_min_j- $mpki_min_k - 4];
-		my $speedup1 = 	$CPIs[$programs{$keys[$program1]}][$length-1]
-					/$CPIs[$programs{$keys[$program1]}][$mpki_min_i] + 
-						$CPIs[$programs{$keys[$program2]}][$length-1]
-					/$CPIs[$programs{$keys[$program2]}][$mpki_min_j] +
-						$CPIs[$programs{$keys[$program3]}][$length-1]
-					/$CPIs[$programs{$keys[$program3]}][$mpki_min_k] +
+		my $speedup1 = 	$CPIs[$programs{$keys[$prog1]}][$length-1]
+					/$CPIs[$programs{$keys[$prog1]}][$mpki_min_i] + 
+						$CPIs[$programs{$keys[$prog2]}][$length-1]
+					/$CPIs[$programs{$keys[$prog2]}][$mpki_min_j] +
+						$CPIs[$programs{$keys[$prog3]}][$length-1]
+					/$CPIs[$programs{$keys[$prog3]}][$mpki_min_k] +
 						$CPIs[$programs{$keys[$prog4]}][$length-1]
 					/$CPIs[$programs{$keys[$prog4]}][$length - $mpki_min_i 
 											- $mpki_min_j- $mpki_min_k - 4];
-		my $speedup2 = 	$CPIs[$programs{$keys[$program1]}][$length-1]
-					/$CPIs[$programs{$keys[$program1]}][$ipc_i] + 
-						$CPIs[$programs{$keys[$program2]}][$length-1]
-					/$CPIs[$programs{$keys[$program2]}][$ipc_j] +
-						$CPIs[$programs{$keys[$program3]}][$length-1]
-					/$CPIs[$programs{$keys[$program3]}][$ipc_k] +
+		my $speedup2 = 	$CPIs[$programs{$keys[$prog1]}][$length-1]
+					/$CPIs[$programs{$keys[$prog1]}][$ipc_i] + 
+						$CPIs[$programs{$keys[$prog2]}][$length-1]
+					/$CPIs[$programs{$keys[$prog2]}][$ipc_j] +
+						$CPIs[$programs{$keys[$prog3]}][$length-1]
+					/$CPIs[$programs{$keys[$prog3]}][$ipc_k] +
 						$CPIs[$programs{$keys[$prog4]}][$length-1]
 					/$CPIs[$programs{$keys[$prog4]}][$length - $ipc_i 
 											- $ipc_j- $ipc_k - 4];
-		my $workload = "$keys[$program1]+$keys[$program2]+"
-						."$keys[$program3]+$keys[$prog4]";
+		my $workload = "$keys[$prog1]+$keys[$prog2]+"
+						."$keys[$prog3]+$keys[$prog4]";
 		# mpki diverge
 		$mpki_diff = $mpki_total2 - $mpki_total1;
 		$absolute_mpki_diverge{$workload} = $mpki_diff;
@@ -158,9 +158,9 @@ for ($program1 = 0; $program1 <= $key_num - 4; $program1++){
 		$absolute_ipc_diverge{$workload} = $ipc_diff;
 		$relative_ipc_diverge{$workload} = $ipc_diff*100/$ipc_total1;
 		# speedup diverge
-		my $speedup_diff = $speedup2-$speedup1;
+		my $speedup_diff = $speedup2 - $speedup1;
 		$absolute_speedup_diverge{$workload} = $speedup_diff;
-		$relative_speedup_diverge{$workload} = 	$speedup_diff*100/$speedup1;
+		$relative_speedup_diverge{$workload} = $speedup_diff*100/$speedup1;
 			}#pg4
 		}#pg3
 	}#pg2
@@ -168,9 +168,8 @@ for ($program1 = 0; $program1 <= $key_num - 4; $program1++){
 
 print "\n-------------------------------------------------------------\n\n";
 my $total = $same_result + $diff_result;
-printf "Total results: %d, diff results: $diff_result\n".
-		"percentage: %.04f%%\n\n", $total,
-			($diff_result)*100/$total;
+printf "Total results: $total, diff results: $diff_result\n".
+		"percentage: %.04f%%\n\n", ($diff_result)*100/$total;
 
 print "Divergent details\n";
 # speedup
@@ -182,17 +181,17 @@ print_avg("[divergent cases]Increase in relative speedup", \@relative_speedup);
 
 # mpki
 my @absolute_mpki = (values %absolute_mpki_diverge);
-print_avg("absolute mpki", \@absolute_mpki, $total);
+print_avg("\nabsolute mpki", \@absolute_mpki, $total);
 my @relative_mpki = (values %relative_mpki_diverge);
 print_avg("[all]Increase in relative mpki", \@relative_mpki, $total);
 print_avg("[divergent cases]Increase in relative mpki", \@relative_mpki);
 
 # ipc
 my @absolute_ipc = (values %absolute_ipc_diverge);
-print_avg("absolute ipc", \@absolute_ipc, $total);
+print_avg("\nabsolute ipc", \@absolute_ipc, $total);
 my @relative_ipc = (values %relative_ipc_diverge);
 print_avg("[all]Increase in relative ipc", \@relative_ipc, $total);
-print_avg("[divergent cases] in relative ipc", \@relative_ipc);
+print_avg("[divergent cases]Increase in relative ipc", \@relative_ipc);
 
 print_top(\%relative_speedup_diverge, "relative speedup", 5, 10,8,6,4,2);
 print_top(\%relative_mpki_diverge, "relative mpki", 5, 50,40,30,20,10,5);
